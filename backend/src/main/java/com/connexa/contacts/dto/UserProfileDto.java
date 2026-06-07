09201b9171ecdd983de3704941bb0489592f72d1
N <nehawaseem846@gmail.com>
2026-06-08 03:48:37 +0500

    User profile dto commit

---
+++ backend/src/main/java/com/connexa/contacts/dto/UserProfileDto.java
@@ -0,0 +1,49 @@
+package com.connexa.contacts.dto;
+
+public class UserProfileDto {
+    private Long id;
+    private String email;
+    private String phone;
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
