9d1648aa9960a494a77dc478653a0ea420d307f5
N <nehawaseem846@gmail.com>
2026-06-08 03:49:08 +0500

    exception commit

---
+++ backend/src/main/java/com/connexa/contacts/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.connexa.contacts.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
